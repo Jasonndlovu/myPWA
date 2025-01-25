7f3056dcc5aeb0a2c0254e999817016894fe65c2
KhanaTechnologies <KhanaTechnologies@gmail.com>
2025-01-25 21:23:49 +0200

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'myPWA',
+  webDir: 'www'
+};
+
+export default config;
